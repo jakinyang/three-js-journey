7c2999af5512d0d142fe2514b25538b4e97fe345
jakinyang <109392405+jakinyang@users.noreply.github.com>
2023-02-20 17:26:00 -0800

    --add lesson 13

--- 13-3d-text/src/script.js
+++ 13-3d-text/src/script.js
@@ -1,5 +1,8 @@
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import * as THREE from 'three';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+
 import * as dat from 'lil-gui'
 
 /**
@@ -19,37 +22,74 @@ const scene = new THREE.Scene()
  */
 const textureLoader = new THREE.TextureLoader()
 
+/*
+* Font Loader
+*/
+
+const fontLoader = new FontLoader();
+fontLoader.load(
+  '/Lobster/Lobster_Regular.json',
+  (font) => {
+    console.log("Fonts Loaded");
+    const textGeometry = new TextGeometry(
+      "MOO (REVOLUTION!)",
+      {
+        font: font,
+        size: 0.5,
+        height: 0.05,
+        curveSegments: 5,
+        bevelEnabled: true,
+        bevelThickness: 0.03,
+        bevelSize: 0.02,
+        bevelOffset: 0,
+        bevelSegments: 2,
+      }
+    );
+    const material = new THREE.MeshBasicMaterial()
+    material.wireframe = false
+    const textMesh =  new THREE.Mesh(textGeometry, material)
+    textMesh.position.set(-3, 0, 0)
+    scene.add(textMesh)
+  }
+)
+
+/*
+* Axes Helper
+*/
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper);
+
 /**
  * Object
  */
 const cube = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1),
-    new THREE.MeshBasicMaterial()
+  new THREE.BoxGeometry(1, 1, 1),
+  new THREE.MeshBasicMaterial()
 )
 
-scene.add(cube)
+// scene.add(cube)
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
@@ -57,9 +97,9 @@ window.addEventListener('resize', () =>
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 1
+camera.position.x = 0
 camera.position.y = 1
-camera.position.z = 2
+camera.position.z = 6
 scene.add(camera)
 
 // Controls
@@ -70,7 +110,7 @@ controls.enableDamping = true
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -80,18 +120,17 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Update controls
+  controls.update()
 
-    // Render
-    renderer.render(scene, camera)
+  // Render
+  renderer.render(scene, camera)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
